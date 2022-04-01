770ddb7329af5662956815db54d56bca2936d933
Michal Mojžiš <missko111@gmail.com>
2022-04-01 14:53:19 +0200

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 import { OsobyStrankaComponent } from './osoby/osoby-stranka/osoby-stranka.component';
+import {NavigationBarComponent} from "./main-page-components/navigation-bar/navigation-bar.component";
+import {MdbCollapseModule} from "mdb-angular-ui-kit/collapse";
 
 @NgModule({
   declarations: [
     AppComponent,
-    OsobyStrankaComponent
+    OsobyStrankaComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
     NgbModule,
-    SweetAlert2Module.forRoot()
+    SweetAlert2Module.forRoot(),
+    MdbCollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/osoby/osoby-stranka/osoby-stranka.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-osoby-stranka',
+  templateUrl: './osoby-stranka.component.html',
+  styleUrls: ['./osoby-stranka.component.css']
+})
+export class OsobyStrankaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
