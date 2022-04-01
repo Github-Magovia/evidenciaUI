8022dae60c4ca2f21054b348d6978091688b0eac
Michal Mojžiš <missko111@gmail.com>
2022-04-01 13:02:48 +0200

    Added Sweetalert2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { OsobyStrankaComponent } from './osoby/osoby-stranka/osoby-stranka.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OsobyStrankaComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    SweetAlert2Module.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
