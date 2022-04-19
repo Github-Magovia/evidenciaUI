196d77818aefd6e309c8ac89f322ad17d96d76a0
ppalo21 <ppavlik48@gmail.com>
2022-04-19 20:13:13 +0200

    ockovanie FE v1.2.3

--- src/models/ockovanie.model.ts
+++ src/models/ockovanie.model.ts
@@ -2,17 +2,19 @@ export class Ockovanie{
   id?: number;
   idPerson: number;
   idVaccine: number;
-  firstName?: String;
-  lastName?: String;
-  type?: String;
+  firstName?: string;
+  lastName?: string;
+  type?: string;
+  dateOfVaccination: string
 
 
-  constructor(id: number, idPerson: number, idVaccine: number, firstName: String, lastName: String, type: String) {
+  constructor(id: number, idPerson: number, idVaccine: number, firstName: string, lastName: string, type: string, dateOfVaccination: string) {
     this.id = id;
     this.idPerson = idPerson;
     this.idVaccine = idVaccine;
     this.firstName = firstName;
     this.lastName = lastName;
     this.type = type;
+    this.dateOfVaccination = dateOfVaccination;
   }
 }
