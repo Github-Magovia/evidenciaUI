ebc04672768023ee0a3ba75632bbeb42e570b4e3
ppalo21 <ppavlik48@gmail.com>
2022-04-23 20:29:55 +0200

    loteria v1

--- src/app/child-components/hlavna/hlavna-stranka/hlavna-stranka.component.ts
+++ src/app/child-components/hlavna/hlavna-stranka/hlavna-stranka.component.ts
@@ -1,8 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {DatePipe} from "@angular/common";
 import {DataService} from "../../../data.service";
 import {CountryData} from "../../../../models/model";
+import {Lottery} from "../../../../models/lottery.model";
+import {FormGroup} from "@angular/forms";
+import {SortEvent, SortLottery} from "./sort-lottery";
 
+
+const compare = (v1: String | number, v2: String | number) => v1 < v2 ? -1 : v1 > v2 ? 1 : 0;
 
 @Component({
   selector: 'app-hlavna-stranka',
@@ -13,6 +18,7 @@ import {CountryData} from "../../../../models/model";
 })
 export class HlavnaStrankaComponent implements OnInit {
 
+  filtering: FormGroup;
   countries: any[];
   country:any;
   confirmed:any;
@@ -21,8 +27,16 @@ export class HlavnaStrankaComponent implements OnInit {
   myDate:any;
   selectedCountryData: CountryData;
   currentDate: string;
-  firstName:any;
-  lastName:any;
+  pageSize: number = 10;
+  collectionSize: number;
+  page: number = 1;
+  @Input() isLoaded: boolean;
+  @Input() lottery: Lottery[] = [];
+  lotterySlice: Lottery[] = [];
+  @ViewChildren(SortLottery) headers: QueryList<SortLottery>;
+
+
+
 
   constructor(private service: DataService) { }
 
@@ -46,6 +60,65 @@ export class HlavnaStrankaComponent implements OnInit {
       this.active=data[index].Active;
       this.death=data[index].Deaths;
     });
+  }
+  private filterOut(): Lottery[] {
+    let filter: any;
+    let filtred: Lottery[] = this.lottery;
+    if(this.filtering.controls['id'].valid) {
+      filter = this.filtering.controls['id'].value.toString();
+      filtred = filtred.filter((lottery: Lottery) => {
+        return lottery.id.toString(10).match(filter);
+      });
+    }
+    if(this.filtering.controls['firstName'].valid){
+      filter = this.filtering.controls['firstName'].value.toString().toLocaleLowerCase();
+      filtred = filtred.filter((lottery: Lottery) => {
+        return lottery.firstName.toLocaleLowerCase().match(filter);
+      });
+    }
+    if(this.filtering.controls['lastName'].valid){
+      filter = this.filtering.controls['lastName'].value.toString().toLocaleLowerCase();
+      filtred = filtred.filter((lottery: Lottery) => {
+        return lottery.lastName.toLocaleLowerCase().match(filter);
+      });
+    }
+    if(this.filtering.controls['amount'].valid) {
+      filter = this.filtering.controls['amount'].value.toString();
+      filtred = filtred.filter((lottery: Lottery) => {
+        return lottery.amount.toString(10).match(filter);
+      });
+    }
+    return filtred;
+  }
+  onSort({column, direction}: SortEvent) {
+    this.headers.forEach(header => {
+      if (header.sortableLottery !== column) { header.direction = ''; }
+    });
+    if (direction === '' || column === '') { this.refreshTable(); }
+    else {
+      this.lotterySlice = [...this.lotterySlice].sort((a, b) => {
+        const res = compare(a[column], b[column]);
+        return direction === 'asc' ? res : -res;
+      });
+    }
+  }
+  refreshTable(): void {
+    this.refreshWinners();
+    if(this.filtering.dirty) {
+      this.lotterySlice = this.filterOut();
+      this.refreshSearchWinners();
+    }
+  }
+
+  private refreshWinners(): void {
+    this.collectionSize = this.lottery.length;
+    this.lotterySlice = this.lottery.map((o, i) => ({id: i + 1, ...o}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
+  private refreshSearchWinners(): void {
+    this.collectionSize = this.lotterySlice.length;
+    this.lotterySlice = this.lotterySlice.map((o, i) => ({id: i + 1, ...o}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
   }
 }
 
